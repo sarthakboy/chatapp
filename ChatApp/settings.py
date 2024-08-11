ada6ccf8b452c7c77015b0e3591a43d34487c849
Sarthak Pandey <pandeysarthak06@gmail.com>
2024-08-11 17:25:28 +0530

    db

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -79,10 +79,20 @@ WSGI_APPLICATION = 'ChatApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'hw8YHUqlC7PX',
+        'HOST': 'ep-cold-fog-a43nu3as-pooler.us-east-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
 
